eea0251b86965e5726bd0eb59dc1c41aebf881b5
2014102a-netizen <2014102a@gmail.com>
2025-11-24 01:15:30 +0300

    Fix Vite entrypoint and base configuration

--- App.tsx
+++ App.tsx
@@ -14,7 +14,6 @@ const App: React.FC = () => {
     restDelta: 0.001
   });
 
-  // Client-side only mounting check
   const [mounted, setMounted] = useState(false);
   useEffect(() => {
     setMounted(true);
@@ -24,7 +23,6 @@ const App: React.FC = () => {
 
   return (
     <main className="min-h-screen bg-black">
-      {/* Scroll Progress Bar */}
       <motion.div
         className="fixed top-0 left-0 right-0 h-1 bg-amber-500 z-50 origin-left"
         style={{ scaleX }}
